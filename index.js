b8a7503863cf0dc7f0abc2b621704a3671baa242
Z Polat <digi.zaf@gmail.com>
2022-10-23 12:17:52 +0200

    reset btn

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+   count += 1
+   countEl.textContent = count
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+   let countStr = count + " - "
+   saveEl.textContent += countStr
+   countEl.textContent = 0
+   count = 0
+}
+
+function reset() {
+   saveEl.textContent = 0
 }
